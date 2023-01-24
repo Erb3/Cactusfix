57ce629053007a2e99c9202b90689d73abb81d6e
Erlend <49862976+Erb3@users.noreply.github.com>
2023-01-24 16:17:16 +0100

    docs: Fix typos in README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Cactusfix
 
 ![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Erb3/Cactusfix?style=for-the-badge)
-![Madefor FabricMC](https://img.shields.io/badge/Made%20for-FabricMC-blue?style=for-the-badge)
+![Made for FabricMC](https://img.shields.io/badge/Made%20for-FabricMC-blue?style=for-the-badge)
 
-Cacti is no longer stronger than lava! Cactusfix makes items not get destoryed by cacti. It also makes caci not damage players / entites. Cactusfix is made for the FabricMC Minecraft modding platform.
+Cacti are no longer stronger than lava! Cactusfix makes items not get destroyed by cacti. It still does damage entities / players, which is intentional. Cactusfix is made for the FabricMC Minecraft modding platform.
--- src/main/java/github/erb3/fabric/cactusfix/mixin/CactusMixin.java
+++ src/main/java/github/erb3/fabric/cactusfix/mixin/CactusMixin.java
@@ -3,6 +3,8 @@ package github.erb3.fabric.cactusfix.mixin;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,11 +15,15 @@ public class CactusMixin {
 
     /**
      * @author Erb3
-     * @reason Disable cactus damage
+     * @reason Disable cactus damage to items
      */
 
     @Overwrite()
     public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
-        return;
+        if (entity instanceof ItemEntity) {
+            return;
+        }
+
+        entity.damage(DamageSource.CACTUS, 1.0F);
     }
 }
