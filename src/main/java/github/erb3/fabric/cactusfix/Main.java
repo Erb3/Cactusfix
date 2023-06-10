66717e7729831857b467cece7cd58310d9bf81d8
Erlend <49862976+Erb3@users.noreply.github.com>
2023-06-10 10:33:02 +0200

    refactor: Rename `betterCactusPlacing`

--- src/main/java/github/erb3/fabric/cactusfix/Main.java
+++ src/main/java/github/erb3/fabric/cactusfix/Main.java
@@ -10,18 +10,21 @@ import org.slf4j.LoggerFactory;
 public class Main implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("cactusfix");
+
     public static final GameRules.Key<GameRules.BooleanRule> SHOULD_CACTUS_DAMAGE_ITEMS =
             GameRuleRegistry.register("shouldCactusDamageItems", GameRules.Category.DROPS,
                     GameRuleFactory.createBooleanRule(false));
-    public static final GameRules.Key<GameRules.BooleanRule> BETTER_CACTUS_PLACING =
-            GameRuleRegistry.register("betterCactusPlacing", GameRules.Category.MISC,
-                    GameRuleFactory.createBooleanRule(false));
+
     public static final GameRules.Key<GameRules.BooleanRule> SHOULD_CACTUS_DAMAGE_PLAYERS =
             GameRuleRegistry.register("shouldCactusDamagePlayers", GameRules.Category.PLAYER,
                     GameRuleFactory.createBooleanRule(true));
 
+    public static final GameRules.Key<GameRules.BooleanRule> BETTER_CACTUS_PLACING =
+            GameRuleRegistry.register("doBetterCactusPlacing", GameRules.Category.MISC,
+                    GameRuleFactory.createBooleanRule(false));
+
     @Override
     public void onInitialize() {
-        LOGGER.info("Hello from Cactusfix!");
+        LOGGER.info("Hello from Cactusfix! Lets get fixing your cactus");
     }
 }
